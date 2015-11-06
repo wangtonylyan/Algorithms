05309ae9d3eaf61b38cb362c3c1c6774da9701a8
wangtonylyan <wangtonylyan@outlook.com>
2015-11-06 11:14:11 +0800

    v0

--- bitwise.py
+++ bitwise.py
@@ -1,75 +1,133 @@
 # -*- coding: utf-8 -*-
 
-#the sign bit really matters in bitwise operations
-
-#cast an integet to signed or unsigned type
+# because the sign bit really matters in bitwise operations
+# provides two functions for casting an integer to signed or unsigned type
 def signed(n): return n
+
+
 def unsigned(n): return n
 
 
-# @problem: count the number of ones in an integer's binary notation
-# @premise: n is an 8-bit unsigned integer
-def countOnesInBinary(n):
-    # python中&、+、>>等操作符的优先级不同于C，要加括号，不然出错
-    n = (n & 85) + ((n >> 1) & 85)  # 85=01010101
-    n = (n & 51) + ((n >> 2) & 51)  # 51=00110011
-    n = (n & 15) + ((n >> 4) & 15)  # 15=00001111
-    return n
+class SettedBitProblem():
+    # @problem: set or reset a certain bit
+    @staticmethod
+    def setOrReset(bitmap, bit):
+        bitmap |= bit  # set
+        bitmap &= ~bit  # reset
 
-
-def countOnesInBinaryTestCase():
-    print countOnesInBinary(85)  # 4
-    print countOnesInBinary(51)  # 4
-    print countOnesInBinary(15)  # 4
-    print countOnesInBinary(1)  # 1
-    print countOnesInBinary(0)  # 0
-    print countOnesInBinary(166)  # 4
-
-
-# @problem: detect whether two signed integers have the same signs
-# @premise: n1 and n2 are of signed integer type
-def detectIfOppositeSigns(n1, n2):
-    return ((n1 ^ n2) < 0) #异或的是两个整型的符号位
-
-
-
-# @premise: n is a non-negative integer
-def theRightmostSetBit(n):
-    # @problem: detect whether an integer is a power of 2
-    def detectIfPowerOfTwo(n):
-        #if n not equals 0 and there's only one bit set
-        return n and not (n &(n-1))
+    # @problem: count the number of ones in an integer's binary notation
+    # @premise: n is an 8-bit unsigned integer
+    @staticmethod
+    def countSettedBitsNumber(n):
+        # python中&、+、>>等操作符的优先级不同于C，要加括号，不然出错
+        n = (n & 85) + ((n >> 1) & 85)  # 85=01010101
+        n = (n & 51) + ((n >> 2) & 51)  # 51=00110011
+        n = (n & 15) + ((n >> 4) & 15)  # 15=00001111
+        return n
 
     # @problem: get the rightmost bit which is set
-    def getRightmostBit(n):
-        return n & (~(n-1))
+    # @premise: n is a non-negative integer
+    @staticmethod
+    def getRightmostSettedBit(n):
+        return n & (~(n - 1))
 
     # @problem: reset the rightmost bit which is set
-    def resetRightmostBit(n):
-        return n & (n-1)
+    # @premise: n is a non-negative integer
+    @staticmethod
+    def resetRightmostSettedBit(n):
+        return n & (n - 1)
 
-# @problem: compute the absolute value of an integer
-# @premise: n is a 32-bit signed integer
-def computeAbsoluteValue(n):
-    #1)
-    ret = n if n >= 0 else -unsigned(n)# this even works on a non-2s-compliment machine
-    #2)
-    mask = n >> 31
-    ret = (n + mask) ^ mask
-    #3)
-    ret = (n ^ mask) - mask
-    return ret
+    # @problem: detect whether an integer is a power of 2
+    # @premise: n is a non-negative integer
+    @staticmethod
+    def detectPowerOfTwo(n):
+        # if n not equals 0 and there's only one bit set
+        return n and not (n & (n - 1))
+
+    # @problem: detect whether two signed integers have the same signs
+    # @premise: n1 and n2 are of signed integer type
+    @staticmethod
+    def detectOppositeSigns(n1, n2):
+        return ((n1 ^ n2) < 0)  # 异或的是两个整型的符号位
+
+    def testCase(self):
+        assert (self.countSettedBitsNumber(85) == 4)
+        assert (self.countSettedBitsNumber(51) == 4)
+        assert (self.countSettedBitsNumber(15) == 4)
+        assert (self.countSettedBitsNumber(1) == 1)
+        assert (self.countSettedBitsNumber(0) == 0)
+        assert (self.countSettedBitsNumber(166) == 4)
+
+        assert (self.detectOppositeSigns(1, 2) == False)
+        assert (self.detectOppositeSigns(-1, -2) == False)
+        assert (self.detectOppositeSigns(1, 0) == False)
+        assert (self.detectOppositeSigns(-1, 0) == True)
+        assert (self.detectOppositeSigns(1, -1) == True)
 
 
-def func(n):
-    bitmap = 85
+class NumberProblem():
+    # @problem: compute the absolute value of an integer
+    # @premise: n is a 32-bit signed integer
+    @staticmethod
+    def computeAbsoluteValue(n):
+        # 1)
+        abs1 = n if n >= 0 else -unsigned(n)  # this even works on a non-2s-compliment machine
+        # 2)
+        mask = n >> 31
+        abs2 = (n + mask) ^ mask
+        # 3)
+        mask = n >> 31
+        abs3 = (n ^ mask) - mask
+        return (abs1, abs2, abs3)
 
-    # 2) set and reset a certain bit
-    bitmap |= bit
-    bitmap &= ~bit
+    # @problem: swap two integers without temporary space
+    @staticmethod
+    def swap(n1, n2):
+        def f1(n1, n2):
+            n1 += n2
+            n2 = n1 - n2
+            n1 = n1 - n2
+            return (n1, n2)
+
+        def f2(n1, n2):
+            n1 -= n2
+            n2 += n1
+            n1 = n2 - n1
+            return (n1, n2)
+
+        def f3(n1, n2):
+            n1 ^= n2
+            n2 ^= n1
+            n1 ^= n2
+            return (n1, n2)
+
+        return (f1(n1, n2), f2(n1, n2), f3(n1, n2))
+
+    # @problem: compute modulus division by a power of 2
+    # @premise: d is a power of 2
+    @staticmethod
+    def modulusByPowerOfTwo(n, d):
+        assert (d > 1 and d & (d - 1) == 0)  # d is a power of 2
+        return n & (d - 1)
+
+    def testCase(self):
+        assert (self.computeAbsoluteValue(1) == (1, 1, 1))
+        assert (self.computeAbsoluteValue(-1) == (1, 1, 1))
+        assert (self.computeAbsoluteValue(0) == (0, 0, 0))
+
+        assert (self.swap(1, 2) == ((2, 1), (2, 1), (2, 1)))
+        assert (self.swap(-1, -2) == ((-2, -1), (-2, -1), (-2, -1)))
+        assert (self.swap(1, 0) == ((0, 1), (0, 1), (0, 1)))
+        assert (self.swap(-1, 0) == ((0, -1), (0, -1), (0, -1)))
+        assert (self.swap(-1, 1) == ((1, -1), (1, -1), (1, -1)))
+
+        assert (self.modulusByPowerOfTwo(15, 2) == 1)
+        assert (self.modulusByPowerOfTwo(16, 4) == 0)
+        assert (self.modulusByPowerOfTwo(18, 8) == 2)
 
 
 if __name__ == '__main__':
-#    countOnesInBinaryTestCase()
-
-    print computeAbsoluteValue(-1)
+    sbp = SettedBitProblem()
+    sbp.testCase()
+    np = NumberProblem()
+    np.testCase()
--- data_structure/hashTable.py
+++ data_structure/hashTable.py
@@ -1,7 +1,76 @@
 # -*- coding: utf-8 -*-
 # data structure: hash table
 
+# 1) collision resolution
+# linear probing: 下一个空位
+# seperate chaining: 链表
+# 2) parallism
+# 第一次哈希入链表，第二次哈希入哈希表
 
+import hash
 
-#collision resolution: 链表、下一个空位
-#parallism: 第一次哈希入链表，第二次哈希入哈希表
+# implementation: linear probing hash table based on parallel arrays
+class HashTable1():
+    def __init__(self):
+        self.hashObj = hash.StringHash()  # the key should be a string
+        # stores keys and values in parallel arrays
+        self.size = 0x01 << 4  # size of arrays
+        self.keys = [None for i in range(self.size)]  # fixed-size array
+        self.vals = [None for i in range(self.size)]  # fixed-size array
+        self.num = 0  # number of tuples in arrays, which is kept in the range [size/8, size/2]
+
+    def _hash(self, key):
+        return self.hashObj.hash3(key) % self.size
+
+    def _resize(self, size):
+        size, self.size = self.size, size
+        keys, self.keys = self.keys, [None for i in range(self.size)]
+        vals, self.vals = self.vals, [None for i in range(self.size)]
+        num, self.num = self.num, 0
+        for i in range(size):
+            if keys[i] != None:
+                self.put(keys[i], vals[i])
+
+    def get(self, key):
+        i = self._hash(key)
+        while self.keys[i] != None:
+            if self.keys[i] == key:
+                return self.vals[i]
+            i = (i + 1) % self.size
+        return None  # values should not be None
+
+    def put(self, key, value):
+        if self.num >= (self.size >> 1):
+            self._resize(self.size << 1)
+        i = self._hash(key)
+        while self.keys[i] != None:
+            if self.keys[i] == key:
+                self.vals[i] = value
+                return
+            i = (i + 1) % self.size
+        self.keys[i] = key
+        self.vals[i] = value
+        self.num += 1
+
+    def delete(self, key):
+        ret = None  # because values never be None, it also indicates whether the target tuple exists
+        i = self._hash(key)
+        while self.keys[i] != None:
+            if self.keys[i] == key:
+                ret = self.vals[i]
+                # set the target tuple to None
+                self.keys[i] = None
+                self.vals[i] = None
+                break
+            i = (i + 1) % self.size
+        if ret != None:
+            i = (i + 1) % self.size
+            while self.keys[i] != None:
+                self.put(key, self.vals[i])  # re-put all tuples after the deleted tuple
+                self.keys[i] = None
+                self.vals[i] = None
+                i = (i + 1) % self.size
+            self.num -= 1
+            if self.num < (self.size >> 3):
+                self._resize(self.size >> 1)
+        return ret
