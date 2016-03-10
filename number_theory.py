0e2002ec7e6e1cd62685f746e4dbaa861663442e
wangtonylyan <wangtonylyan@outlook.com>
2016-03-10 17:34:44 +0800

    add prime algo

---
+++ number_theory.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+
+
+# prime
+def Sieve_of_Eratosthenes(num):
+    lst = [1 for i in range(num + 1)]
+    for i in range(2, len(lst) / 2 + 1):
+        if lst[i] == 1:
+            j = i
+            while i * j < len(lst):
+                lst[i * j] = 0
+                j += 1
+    ret = []
+    for i in range(2, len(lst)):
+        if lst[i] == 1:
+            ret.append(i)
+    return ret
+
+
+if __name__ == '__main__':
+    print Sieve_of_Eratosthenes(100)
