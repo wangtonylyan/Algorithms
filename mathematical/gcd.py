4d281642f849512c043d8c2d97db8ab3814dfc27
wang.miao <CHT1HTSH3191@alpsgroup.ad.local>
2016-03-16 11:25:27 +0800

    update mathemathics

--- mathematical/gcd.py
+++ mathematical/gcd.py
@@ -3,12 +3,14 @@
 
 def Euclidean(a, b):
     x, y = max(a, b), min(a, b)
-    while x > 0 and y > 0 and x % y != 0:
-        x = x % y
-        x, y = y, x
-    return y
+    while y != 0:
+        x, y = y, x % y
+    return x
 
 
 if __name__ == '__main__':
+    assert (Euclidean(1, 3) == 1)
+    assert (Euclidean(10, 20) == 10)
+    assert (Euclidean(13, 7) == 1)
     assert (Euclidean(945, 2415) == 105)
     print 'done'
