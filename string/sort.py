72441108e448b7842bc92adffbd97f62f31a138b
wangtonylyan <wangtonylyan@outlook.com>
2016-04-27 17:29:36 +0800

    update string sort

--- string/sort.py
+++ string/sort.py
@@ -1,6 +1,10 @@
 # -*- coding: utf-8 -*-
 # problem: string sorting
 # solution: lease-significant-digit-first (LSD), most-significant-digit-first (MSD)
+# LSD和MSD算法思想与radix sort完全相同，都是基于counting sort
+# 且习惯上都将字符串和数字的首位视作MSD，但不同的是
+# 较短字符串的LSD被''填充，而较短数字的MSD被0填充
+
 
 import random
 
@@ -9,8 +13,8 @@ class StringSort(object):
     def __init__(self):
         self.alphabet = 256  # number of legal characters by ASCII, [1,255]
 
-    # @param type: 0==fixed length string, 1==variable length string
-    def testcase(self, type):
+    # @param type: 0==variable length string, 1==fixed length string
+    def testcase(self, type=0):
         def test(case):
             ret = self.main(case[:])
             case.sort()
@@ -21,11 +25,11 @@ class StringSort(object):
 
         cases = []
         for i in range(20):
-            width = random.randint(1, 25) if type == 0 else 0
+            width = random.randint(1, 25) if type == 1 else 0
             case = []
             for j in range(20):
                 s = ''
-                width = width if type == 0 else random.randint(1, 25)
+                width = width if type == 1 else random.randint(1, 25)
                 for k in range(width):
                     s += chr(random.randint(ord('a'), ord('z')))
                 case.append(s)
@@ -35,45 +39,16 @@ class StringSort(object):
         print 'pass:', self.__class__
 
 
-class LSDFixed(StringSort):
+class LSD(StringSort):
     def __init__(self):
-        super(LSDFixed, self).__init__()
-
-    def main(self, lst):
-        assert (reduce(lambda x, y: x and len(lst[0]) == len(y), lst[1:], True))  # fixed length
-        width = len(lst[0])
-        for w in range(width - 1, -1, -1):
-            # 1) compute frequency counts
-            cnt = [0] * self.alphabet
-            for i in lst:
-                cnt[ord(i[w]) + 1] += 1
-            # 2) transfrom counts to indices
-            for i in range(len(cnt) - 1):
-                cnt[i + 1] += cnt[i]
-            # 3) distribute the records from lst to aux
-            aux = [None] * len(lst)
-            for i in lst:
-                aux[cnt[ord(i[w])]] = i
-                cnt[ord(i[w])] += 1
-            # 4) store result back into lst by exchanging both references
-            lst, aux = aux, lst
-        return lst
-
-    def testcase(self):
-        super(LSDFixed, self).testcase(0)
-
-
-class LSDVariable(StringSort):
-    def __init__(self):
-        super(LSDVariable, self).__init__()
+        super(LSD, self).__init__()
 
     def main(self, lst):
         width = max(map(lambda x: len(x), lst))
         for w in range(width - 1, -1, -1):
             cnt = [0] * self.alphabet
             for i in lst:
-                # 较短字符串通常都是根据实际情况
-                # 利用空闲的cnt[0]或cnt[-1]来统计
+                # 较短字符串通常都是根据实际情况，利用空闲的cnt[1]或cnt[-1]来统计
                 if w >= len(i):
                     cnt[0 + 1] += 1
                 else:
@@ -92,7 +67,7 @@ class LSDVariable(StringSort):
         return lst
 
     def testcase(self):
-        super(LSDVariable, self).testcase(1)
+        super(LSD, self).testcase()
 
 
 class MSD(StringSort):
@@ -100,36 +75,35 @@ class MSD(StringSort):
         super(MSD, self).__init__()
 
     def main(self, lst):
-        def recur(low, high, ind):
-            if high - low < 2 or ind >= max(map(lambda x: len(x), lst[low:high])):
+        def recur(low, high, wid):
+            if high - low < 2 or wid >= max(map(lambda x: len(x), lst[low:high])):
                 return
             cnt = [0] * self.alphabet
-            # 由于只涵盖了[1,255]，因此cnt[1]可以用来统计长度不大于ind的字符串个数
-            # 否则所有ASCII字符的索引值偏移量都将变为2
             for i in lst[low:high]:
-                cnt[ord(i[ind]) + 1 if ind < len(i) else 0 + 1] += 1
+                # 由于只涵盖了[1,255]，因此空闲的cnt[1]可以用来统计长度较短的字符串个数
+                # 否则所有ASCII字符的索引值偏移量都将变为2
+                cnt[ord(i[wid]) + 1 if wid < len(i) else 0 + 1] += 1
             for i in range(len(cnt) - 1):
                 cnt[i + 1] += cnt[i]
             aux = [None] * (high - low)
             for i in lst[low:high]:
-                aux[cnt[ord(i[ind]) if ind < len(i) else 0]] = i
-                cnt[ord(i[ind]) if ind < len(i) else 0] += 1
+                aux[cnt[ord(i[wid]) if wid < len(i) else 0]] = i
+                cnt[ord(i[wid]) if wid < len(i) else 0] += 1
             lst[low:high] = aux
 
-            assert (max(map(lambda x: len(x), lst[low:low + cnt[0]])) <= ind if cnt[0] > 0 else True)
+            assert (max(map(lambda x: len(x), lst[low:low + cnt[0]])) <= wid if cnt[0] > 0 else True)
             for i in range(len(cnt) - 1):
-                recur(low + cnt[i], low + cnt[i + 1], ind + 1)
+                recur(low + cnt[i], low + cnt[i + 1], wid + 1)
             assert (cnt[-1] == high - low)
 
         recur(0, len(lst), 0)
         return lst
 
     def testcase(self):
-        super(MSD, self).testcase(1)
+        super(MSD, self).testcase()
 
 
 if __name__ == '__main__':
-    LSDFixed().testcase()
-    LSDVariable().testcase()
+    LSD().testcase()
     MSD().testcase()
     print 'done'
