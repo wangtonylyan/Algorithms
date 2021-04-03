cbbb8d41f49cd48ab64325ac723d0a8770094c4c
TonyLYan <wangtonylyan@outlook.com>
2021-04-03 11:22:06 +0800

    start moving code to Notion

--- divide_and_conquer/mergeSort.py
+++ divide_and_conquer/mergeSort.py
@@ -4,85 +4,6 @@ import random
 from number.sort import Sort
 
 
-class MergeSort(Sort):
-    def __init__(self):
-        super(MergeSort, self).__init__()
-        self.funcs.append(self.main_1)
-        self.funcs.append(self.main_2)
-        self.funcs.append(self.main_3)
-
-    @staticmethod
-    def _merge(lst, low, mid, high):  # [low,high)
-        # auxliary space
-        t1 = lst[low:mid]
-        t2 = lst[mid:high]
-        i, j = 0, 0
-        while i < len(t1) and j < len(t2):
-            if t1[i] <= t2[j]:
-                lst[low + i + j] = t1[i]
-                i += 1
-            else:
-                lst[low + i + j] = t2[j]
-                j += 1
-        if i < len(t1):
-            assert (j == len(t2))
-            lst[low + i + j:high] = t1[i:]
-        else:
-            assert (i == len(t1) and j < len(t2))
-            lst[low + i + j:high] = t2[j:]
-
-    # bottom-up, iterative
-    def main_1(self, lst):
-        step = 1
-        while step < len(lst):
-            i = 0
-            while i + step + step <= len(lst):
-                self._merge(lst, i, i + step, i + step + step)
-                i += step + step
-            # 以下处理边界情况
-            if i + step < len(lst):
-                self._merge(lst, i, i + step, len(lst))
-            step *= 2
-        return lst
-
-    # top-down, iterative
-    def main_2(self, lst):
-        stk = [(0, len(lst))]
-        top = 0
-        while top < len(stk):
-            low, high = stk[top]
-            top += 1
-            if high - low < 2:
-                continue
-            mid = low + (high - low) / 2
-            stk.append((low, mid))
-            stk.append((mid, high))
-
-        assert (len(stk) % 2 == 1)
-        for i in range(len(stk) - 1, 0, -2):
-            assert (stk[i - 1][1] == stk[i][0])
-            self._merge(lst, stk[i - 1][0], stk[i - 1][1], stk[i][1])
-        return lst
-
-    # top-down, recursive
-    def main_3(self, lst):
-        def _merge(lst1, lst2):
-            if len(lst1) == 0:
-                return lst2
-            elif len(lst2) == 0:
-                return lst1
-            if lst1[0] <= lst2[0]:
-                return [lst1[0]] + _merge(lst1[1:], lst2)
-            else:
-                return [lst2[0]] + _merge(lst1, lst2[1:])
-
-        # recursion and merge
-        if len(lst) < 2:
-            return lst
-        mid = len(lst) >> 1
-        return _merge(self.main_3(lst[:mid]), self.main_3(lst[mid:]))
-
-
 # @problem: Given two sorted arrays A and B, A has a large enough buffer
 # at the end to hold B. Merge B into A in sorted order.
 class InPlaceMerge():
@@ -142,6 +63,7 @@ class CountInversionsInArray():
                     lst[low + i + j] = t2[j]
                     j += 1
                     count += len(t1) - i  # the only difference with MergeSort
+
             if i == mid - low:
                 lst[low + i + j:high] = t2[j:]
             else:
@@ -175,78 +97,8 @@ class CountInversionsInArray():
 
         cases = [[1], [1, 2], [2, 1]]
         for _ in range(20):
-            lst = [random.randint(0, 100) for i in range(random.randint(5, 50))]
-            random.shuffle(lst)
-            cases.append(lst)
-        map(test, cases)
-        print 'pass:', self.__class__
-
-
-# @problem: sort one linked list
-# 合并排序相比于其他排序，无需过多的随机访问
-# 因此非常适用于链表排序
-class SortLinkedList():
-    class Node():
-        def __init__(self, value, next=None):
-            self.value = value
-            self.next = next
-
-    def _insert(self, lst, value):
-        return self.__class__.Node(value, lst) if lst else self.__class__.Node(value)
-
-    def main(self, lst):
-        def _split(lst):
-            slow = lst  # tortoise
-            fast = lst.next  # hare
-            while fast != None:
-                fast = fast.next
-                if fast:
-                    # update slow pointer here
-                    slow = slow.next
-                    fast = fast.next
-            # so that slow is before the midpoint in the list
-            ret = slow.next
-            # then split the list into two
-            slow.next = None
-            return ret
-
-        def _merge(lst1, lst2):
-            if lst1 == None:
-                return lst2
-            elif lst2 == None:
-                return lst1
-            if lst1.value <= lst2.value:
-                lst1.next = _merge(lst1.next, lst2)
-                return lst1
-            else:
-                lst2.next = _merge(lst1, lst2.next)
-                return lst2
-
-        # recursion and merge
-        if lst == None or lst.next == None:
-            return lst
-        mid = _split(lst)
-        lst = self.main(lst)
-        mid = self.main(mid)
-        return _merge(lst, mid)
-
-    def testcase(self):
-        def test(lst):
-            llst = None
-            for i in lst:
-                llst = self._insert(llst, i)
-            llst = self.main(llst)
-            lst.sort()
-            i = 0
-            while llst and i < len(lst):
-                assert (llst.value == lst[i])
-                llst = llst.next
-                i += 1
-            assert (llst == None and i == len(lst))
-
-        cases = [[1], [1, 2], [2, 1]]
-        for _ in range(20):
-            lst = [random.randint(0, 100) for _ in range(random.randint(5, 50))]
+            lst = [random.randint(0, 100)
+                   for i in range(random.randint(5, 50))]
             random.shuffle(lst)
             cases.append(lst)
         map(test, cases)
@@ -254,8 +106,7 @@ class SortLinkedList():
 
 
 if __name__ == '__main__':
-    MergeSort().testcase()
     InPlaceMerge().testcase()
     CountInversionsInArray().testcase()
-    SortLinkedList().testcase()
-    print 'done'
+
+    print('done')
